2e7b2faaa47eba2736841c9f3daeeb007e797b9e
MingBit <angela.wu1101@gmail.com>
2020-06-03 18:09:32 +0200

    organise code

---
+++ src/model_compare.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  9 14:16:47 2019
+@author: mwu
+"""
+
+from __future__ import absolute_import
+import pandas as pd
+import numpy as np
+import itertools
+import copy
+import seaborn as sns
+from sklearn import metrics
+pd.set_option('mode.chained_assignment', None)
+
+
+def read_data(file_path, dropRate=0):
+    Sim = pd.read_csv(file_path + '/ExpressionData.csv', sep=',', index_col=0)
+    Sim_Ref_raw = pd.read_csv(file_path + '/refNetwork.csv', sep=',', index_col=None)
+
+    Sim_Ref_raw['weight'] = Sim_Ref_raw['Type'].map({'+': 1, '-': -1})
+    Sim_Ref_raw = Sim_Ref_raw.drop(columns='Type')
+    Sim_Ref_raw.columns = ['source', 'target', 'weight']
+
+    Full_Ref = pd.DataFrame(itertools.permutations(Sim.index, 2), columns=['source', 'target'])
+    Sim_Ref = pd.merge(Full_Ref, Sim_Ref_raw, how='outer').fillna(0)
+
+    Sim_Ref_dropout = copy.deepcopy(Sim_Ref)
+    pos = list(Sim_Ref_dropout[abs(Sim_Ref_dropout.weight) == 1].index)
+    replaceNum = np.random.choice(pos, int(len(pos) * dropRate), replace=False)
+    Sim_Ref_dropout.loc[replaceNum, 'weight'] = 0
+
+    return [Sim, Sim_Ref, Sim_Ref_dropout, Sim_Ref_raw.shape[0]]
+
+
+def remove_duplicate(links):
+    links_list = sorted(links[['source', 'target']].values.tolist())
+    for i in range(len(links_list)):
+        links_list[i] = tuple(sorted(links_list[i]))
+    nodes = pd.DataFrame(list(set(links_list)), columns=('source', 'target'))
+    links = pd.merge(links, nodes, how='right')
+
+    return links
+
+
+def mapping_edges(df_1, df_2, df_1_col_1, df_1_col_2, df_2_col_1, df_2_col_2):
+
+    df_1['tmp1'] = df_1[df_1_col_1] + '_' + df_1[df_1_col_2]
+    df_2['tmp1'] = df_2[df_2_col_1] + '_' + df_2[df_2_col_2]
+
+    return len(set(df_1['tmp1']) & set(df_2['tmp1']))
+
+
+def evaluation(links, Ref_links, Num_Genes):
+    # links_filtered=links.loc[(abs(links['value']) > threshold) & (links['var1'] != links['var2'])]
+
+    Detected = links.shape[0]
+    Ref_links = Ref_links[Ref_links.weight != 0]
+    TP = mapping_edges(links, Ref_links, 'source', 'target', 'source', 'target')
+    FN = Ref_links.shape[0] - TP
+    FP = Detected - TP
+    TN = (Num_Genes * Num_Genes) - Ref_links.shape[0] - Detected + TP
+
+    Precision = TP / (TP + FP)
+    Recall = TP / (TP + FN)
+    FDR = FP / (TN + FP)
+
+    F1_Score = (2 * Precision * Recall) / (Precision + Recall)
+    print('Detected:', Detected)
+    print('TP:', TP, '\n', 'FN:', FN, '\n', 'FP:', FP, '\n', 'TN:', TN)
+
+    print('Precision:', Precision, '\n', 'Recall:', Recall, '\n',
+          'F1 Score:', F1_Score, '\n', 'FDR:', FP / (TN + FP))
+    return Detected, TP, FN, FP, TN, Precision, Recall, FDR, F1_Score
+
+
+def test_run(links, Ref_links, input_dataset, filename=None):
+    Detected, TP, FN, FP, TN, Precision, Recall, FDR, F1_Score = evaluation(links, Ref_links, input_dataset.shape[0])
+
+    Comp_Links = pd.merge(links, Ref_links, on=['source', 'target'], how='right').fillna(0)
+
+    auc = metrics.roc_auc_score(np.array(Comp_Links['weight_y'].abs()), np.array(Comp_Links['weight_x'].abs()))
+    fpr, tpr, threshold_1 = metrics.roc_curve(Comp_Links['weight_y'].abs(), Comp_Links['weight_x'].abs())
+    pre, recall, threshold_2 = metrics.precision_recall_curve(Comp_Links['weight_y'].abs(),
+                                                              Comp_Links['weight_x'].abs())
+    # avg_pre = metrics.average_precision_score(Comp_Links['weight_y'].abs(), Comp_Links['weight_x'].abs())
+    avg_pre_auc = metrics.auc(recall, pre)
+
+    return [fpr, tpr, pre, recall, auc, avg_pre_auc, Precision, Recall, F1_Score]
+
+
+def build_curves(ax, node_dict_list, GENIE3_dict, PIDC_dict, GRNBOOST2_dict,
+                 curve, filename, p, q, dropRate):
+    keywords = ['fpr', 'tpr'] if curve == 'ROC' else ['recall_list', 'pre']
+    fill = 1 if curve == 'ROC' else 0
+    node_fpr_pre_df = pd.DataFrame(node_dict_list[i][keywords[0]] for i in range(len(node_dict_list))).fillna(fill)
+    node_tpr_recall_df = pd.DataFrame(node_dict_list[i][keywords[1]] for i in range(len(node_dict_list))).fillna(fill)
+    node_auc_list = list(node_dict_list[i]['auc'] for i in range(len(node_dict_list)))
+    node_avgpre_list = list(node_dict_list[i]['avg_pre'] for i in range(len(node_dict_list)))
+
+    colors = sns.color_palette().as_hex() + sns.color_palette('hls', 8).as_hex()
+
+    for i in range(len(node_dict_list)):
+        ax.plot(node_dict_list[i][keywords[0]], node_dict_list[i][keywords[1]], color='grey', alpha=0.2)
+
+    fpr_dict = {'GENIE3': GENIE3_dict[keywords[0]], 'PIDC': PIDC_dict[keywords[0]],
+                'GRNBOOST2': GRNBOOST2_dict[keywords[0]],
+                'Node2Vec': node_fpr_pre_df.mean(0)}
+    tpr_dict = {'GENIE3': GENIE3_dict[keywords[1]], 'PIDC': PIDC_dict[keywords[1]],
+                'GRNBOOST2': GRNBOOST2_dict[keywords[1]],
+                'Node2Vec': node_tpr_recall_df.mean(0)}
+    auc_dict = {'GENIE3': GENIE3_dict['auc'], 'PIDC': PIDC_dict['auc'], 'GRNBOOST2': GRNBOOST2_dict['auc'],
+                'Node2Vec': np.mean(node_auc_list)}
+    avgpre_dict = {'GENIE3': GENIE3_dict['avg_pre'], 'PIDC': PIDC_dict['avg_pre'],
+                   'GRNBOOST2': GRNBOOST2_dict['avg_pre'],
+                   'Node2Vec': np.mean(node_avgpre_list)}
+
+    for i, color in zip(list(auc_dict.keys()), colors):
+        ax.plot(fpr_dict[i], tpr_dict[i],
+                label='ROC curve {0} (area = {1:0.2f})'.format(i, auc_dict[i]) if curve == 'ROC' else
+                'PR curve {0} (area = {1:0.2f})'.format(i, avgpre_dict[i]),
+                color=color)
+    if curve == 'ROC':
+        ax.plot([0, 1], [0, 1], 'k--')
+
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.0])
+    ax.set_xlabel('False Positive Rate' if curve == 'ROC' else 'Recall', fontsize=20)
+    ax.set_ylabel('True Positive Rate' if curve == 'ROC' else 'Precision', fontsize=20)
+    ax.set_title(filename + '_p_' + str(p) + '_q_' + str(q) + '_dropRate_' + str(dropRate), fontsize=12)
+    ax.legend(loc="lower right")
+
+
+def build_plot(ax, node_dict_list, GENIE3_dict, PIDC_dict, GRNBOOST2_dict):
+    node_precision_list = list(node_dict_list[i]['precision'] for i in range(len(node_dict_list)))
+    node_recall_list = list(node_dict_list[i]['recall'] for i in range(len(node_dict_list)))
+    node_f1score_list = list(node_dict_list[i]['f1_score'] for i in range(len(node_dict_list)))
+
+    Algorithms = ['GENIE3', 'PIDC', 'GRNBOOST2', 'Node2Vec']
+    Eva_Methods = ['Precision', 'Recall', 'F1_Score']
+
+    data = [[GENIE3_dict['precision'], PIDC_dict['precision'], GRNBOOST2_dict['precision'],
+             np.mean(node_precision_list)],
+            [GENIE3_dict['recall'], PIDC_dict['recall'], GRNBOOST2_dict['recall'],
+             np.mean(node_recall_list)],
+            [GENIE3_dict['f1_score'], PIDC_dict['f1_score'], GRNBOOST2_dict['f1_score'],
+             np.mean(node_f1score_list)]]
+
+    colors = sns.color_palette().as_hex() + sns.color_palette('hls', 8).as_hex()
+    X = np.arange(4)
+    for i in range(len(data)):
+        print(data[i])
+        ax.bar(X + 0.25 * i, data[i], color=colors[i], width=0.25)
+
+    ax.set_xlabel('Algorithms', fontsize=25)
+    ax.set_ylabel('Performance', fontsize=25)
+    ax.set_xticks(X + 0.25)
+    ax.set_xticklabels(Algorithms, fontsize=20)
+    ax.legend(Eva_Methods, loc='upper left')
